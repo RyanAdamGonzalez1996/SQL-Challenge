471e5db846c99dc7742f24015ef22d51818f91fb
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-11-03 15:41:28 -0500

    Completed query 5 and added image of output to 'Images'.

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -32,3 +32,10 @@ JOIN dept_manager dm
 ON (e.emp_ID = dm.emp_ID)
 JOIN departments d
 ON (dm.dept_ID = d.dept_ID);
+
+/*5. List first name, last name, and sex for employees whose
+first name is "Hercules" and last names begin with "B." */
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE (first_name = 'Hercules') AND last_name LIKE 'B%';
