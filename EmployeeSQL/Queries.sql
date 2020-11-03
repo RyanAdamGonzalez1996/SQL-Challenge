54fe48fcfd7d6ce92f88b78981610fa79a9eb988
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-11-03 15:16:18 -0500

    Completed query 3 and added image of output to 'Images'.

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,7 +1,24 @@
-/*List the following details of each employee: 
+/*1. List the following details of each employee: 
 employee number, last name, first name, sex, and salary. */
 
 SELECT e.emp_ID, e.last_name, e.first_name, e.sex, s.emp_Id
 FROM salary s
 JOIN employees e
 ON (s.emp_ID = e.emp_ID)
+
+/*2. List first name, last name, and hire date 
+for employees who were hired in 1986.*/
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
+
+/*3. List the manager of each department with the following information: 
+department number, department name, the manager's employee number, last name, first name.*/
+
+SELECT d.dept_ID, d.dept_name, e.emp_ID, e.last_name, e.first_name
+FROM employees e
+JOIN dept_manager dm
+ON (e.emp_ID = dm.emp_ID)
+JOIN departments d
+ON (dm.dept_ID = d.dept_ID);
