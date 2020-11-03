f171a4926299763618a22ac8df19001312b11e89
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-11-03 15:52:44 -0500

    Completed query 7 and added image of output to 'Images'.

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -54,5 +54,13 @@ WHERE (d.dept_name = 'Sales');
 /*7. List all employees in the Sales and Development departments, 
 including their employee number, last name, first name, and department name. */
 
+SELECT e.emp_ID, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de
+ON (e.emp_ID = de.emp_ID)
+JOIN departments d
+ON (de.dept_ID = d.dept_ID)
+WHERE (d.dept_name = 'Sales') OR (d.dept_name = 'Development');
+
 /*8. In descending order, list the frequency count of employee last names, 
 i.e., how many employees share each last name. */
